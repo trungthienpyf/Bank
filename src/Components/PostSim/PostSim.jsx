f9b84af9bc5cbe17cd5d7c6cb53873c41191168e
Nguyen Trung Thien <269.tdd@gmail.com>
2022-12-16 23:37:37 +0700

    update

--- src/Components/PostSim/PostSim.jsx
+++ src/Components/PostSim/PostSim.jsx
@@ -81,7 +81,7 @@ const PostSim = () => {
     };
     window.echo = new Echo({
       broadcaster: "socket.io",
-      host: window.location.hostname + ":6001",
+      // host: window.location.hostname + ":6001",
     });
 
     window.echo
